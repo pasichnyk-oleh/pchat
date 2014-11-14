3aee9fd3e6784bebaf37a8bd3b40b06164cfbd02
djpasica <djpasica92@gmail.com>
2014-11-14 17:45:04 +0200

    settings for heroku db

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@
 __author__ = 'o.pasichnyk'
 
 import os
+import importlib
 
 
 PROJECT_DIR = os.path.join(os.path.dirname(__file__))
@@ -33,3 +34,25 @@ SOCKET_URL = r"/chat/socket"
 # settings for testing
 TESTING_PORT = 8889 #port for unit test
 TESTING_SITE_URL = '0.0.0.0' #url for tests requests
+
+
+#import env settings
+env_name = os.environ.get('ENV_NAME', None)
+
+def import_env_vars(env_settings):
+    globals_var = globals()
+
+    for item in dir(env_settings):
+        if item.startswith("__"):
+            continue
+
+        if item in globals_var:
+            print(item, getattr(env_settings, item))
+            globals_var[item] = getattr(env_settings, item)
+
+try:
+    env_settings = importlib.import_module('settings_%s' % env_name)
+except ImportError:
+    pass
+else:
+    import_env_vars(env_settings)
---
+++ settings_heroku.py
@@ -0,0 +1,13 @@
+# coding: utf-8 -*-
+
+__author__ = 'o.pasichnyk'
+
+DEBUG = False
+
+DATABASE = {
+    'name': 'd7snq90jp63ig7',
+    'host': 'ec2-54-204-39-187.compute-1.amazonaws.com',
+    'user': 'kgxueqzlwnupvk',
+    'password': 'Ria5cEsXnjcTWVMZ8Vyr-sdkpK',
+    'port': 5432
+}
