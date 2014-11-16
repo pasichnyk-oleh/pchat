450abf2d2a7d798e49ac7701581b6220ddf7dc6e
djpasica <djpasica92@gmail.com>
2014-11-16 19:34:01 +0200

    docs for postgre connection

--- utils/db.py
+++ utils/db.py
@@ -7,21 +7,29 @@ from sqlalchemy.orm import sessionmaker
 __author__ = 'o.pasichnyk'
 __all__ = ['PostgreSqlConnect', ]
 
+
 class _Singleton(object):
-  _instance = None
+    '''Class that realize singleton pattern'''
+    _instance = None
 
-  def __new__(class_, *args, **kwargs):
-    if not isinstance(class_._instance, class_):
-        class_._instance = object.__new__(class_, *args, **kwargs)
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_, *args, **kwargs)
 
-    return class_._instance
+        return class_._instance
 
 
 class PostgreSqlConnect(_Singleton):
-    engine = None
-    session = None
+    '''Connect to PostgreSQL server using sqlalchemy'''
+    engine = None #sqlalchemy engine tool
+    session = None #sqlalchemy seesin tool
 
     def __init__(self, db_settings={}, debug=False):
+        '''
+        :param db_settings: dict - conntect settings
+        :param debug: bool - True to show debug info, otherwise False
+        :return: None
+        '''
         Base = declarative_base()
 
         self.engine = create_engine('postgresql://{user}:{password}@{host}:{port}/{name}'.format(**db_settings))
