8a1788ec0537de17d7e7fb260900a73e1170c66e
djpasica <djpasica92@gmail.com>
2014-11-16 19:43:59 +0200

    module docs for db connection

--- db_connect.py
+++ db_connect.py
@@ -1,4 +1,10 @@
 # coding: utf-8 -*-
+'''
+Wrapping SQLAlchemy PostgreSQL connection
+
+:param db: correspond sqlalchemy.session
+:param db_engine: correspond sqlalchemy.engine
+'''
 
 import settings
 from utils.db import PostgreSqlConnect
@@ -6,6 +12,6 @@ from utils.db import PostgreSqlConnect
 __author__ = 'o.pasichnyk'
 __all__ = ['db', 'db_engine', ]
 
-_db_connect = PostgreSqlConnect(settings.DATABASE)
+_db_connect = PostgreSqlConnect(settings.DATABASE) #do connect
 db = _db_connect.session
 db_engine = _db_connect.engine
