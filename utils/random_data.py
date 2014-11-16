9d033bd54ba4c015fe9ccfbb1045d7af795786d9
djpasica <djpasica92@gmail.com>
2014-11-16 19:37:18 +0200

    added docs for string random generator

--- utils/random_data.py
+++ utils/random_data.py
@@ -8,4 +8,11 @@ __all__ = ['random_string', ]
 
 
 def random_string(size=6, chars=string.ascii_uppercase + string.digits):
+    '''
+    Generate random string
+
+    :param size: int - size of string to generate
+    :param chars: list - chars to use in random generating
+    :return: str - generated random string
+    '''
     return ''.join(random.choice(chars) for _ in range(size))
